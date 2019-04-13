7d2642ed5c3c1c24897965dfbb642ea6d5581f28
Rommel Paras <rpparas@gmail.com>
2019-04-13 07:31:49 -0700

    Changed code to pull data from Alpha Advantage
    Downloaded the API output as CSV instead of JSON to avoid extra overhead from parsing nested JSON

--- TickerChart.py
+++ TickerChart.py
@@ -1,13 +1,22 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 import pandas as pd
+import json
+from pandas.io.json import json_normalize
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+apiKey = 'JQKUZMZK74N9U4KY'
+apiUrl = 'https://www.alphavantage.co/query?function='
+params = '&symbol=MSFT&datatype=csv&apikey=' + apiKey
+timeseries = apiUrl + 'TIME_SERIES_DAILY' + params
 
-trace = go.Ohlc(x=df['Date'],
-                open=df['AAPL.Open'],
-                high=df['AAPL.High'],
-                low=df['AAPL.Low'],
-                close=df['AAPL.Close'])
+df = pd.read_csv(timeseries)
+df.sort_values(by=['timestamp']) # sort entries by date (from oldest to newest)
+
+
+trace = go.Ohlc(x=df['timestamp'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])
 data = [trace]
 py.plot(data, filename='simple_candlestick.html')
