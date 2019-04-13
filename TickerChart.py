c288d660d08945d70e0272447a6e3e8cf47712e3
Rommel Paras <rpparas@gmail.com>
2019-04-13 08:59:04 -0700

    Added function to request ticker from User
    User input can be supplied via the command line when running the program or during program execution at runtime.
    
    Also added function to validate ticker input by querying Alpha Advantage and grabbing ticker suggestions if available

--- TickerChart.py
+++ TickerChart.py
@@ -1,11 +1,56 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 import pandas as pd
+import sys
 
-def requestData():
+def getTicker():
+    if len(sys.argv) == 2:
+        ticker = sys.argv[1]
+    else:
+        ticker = requestUserInput()
+
+    tickerStatus = isTickerValid(ticker)
+    while tickerStatus != 1:
+        print('Your entry doesn\'t appear to be a valid ticker.')
+        if tickerStatus == -1:
+            suggestions = suggestMatchingTickers(ticker)
+            if len(suggestions) > 0:
+                print(f'Here are some suggestions: {suggestions}')
+        ticker = requestUserInput()
+        tickerStatus = isTickerValid(ticker)
+
+    return ticker
+
+
+def requestUserInput():
+    ticker = input("Enter a Ticker symbol: ")
+    print("Please hold on while while we look that up ...")
+    return ticker
+
+# returns 1 if ticker is an exact match, 0 if there are no matching tickers, -1 if there are ticker suggestions
+def isTickerValid(ticker):
+    apiKey = 'JQKUZMZK74N9U4KY'
+    requestUrl = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}&apikey=' + apiKey
+    df = pd.read_json(requestUrl)
+    if df.empty:
+        return -1
+    firstMatch = df['bestMatches'].iloc[0]['1. symbol']
+    return 1 if firstMatch == ticker else 0
+
+def suggestMatchingTickers(df):
+    apiKey = 'JQKUZMZK74N9U4KY'
+    requestUrl = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}&apikey=' + apiKey
+    df = pd.read_json(requestUrl)
+    if df.empty:
+        return None
+    return list(df['bestMatches']['1. symbol'])
+
+
+# This function assumes that ticker has already been verified as valid, otherwise, we need to add error-checking
+def requestData(ticker):
     apiKey = 'JQKUZMZK74N9U4KY'
     apiUrl = 'https://www.alphavantage.co/query?function='
-    commonParam = '&symbol=MSFT&datatype=csv&apikey=' + apiKey
+    commonParam = f'&symbol={ticker}&datatype=csv&apikey=' + apiKey
     endpoints = {
         # daily opening, high, low and closing prices for a ticker
         'timeseries': {'fxn': 'TIME_SERIES_DAILY', 'x-axis': 'timestamp'},
@@ -43,5 +88,6 @@ def plotChart(data):
     py.plot(data, filename='ticker-chart.html')
 
 
-data = requestData()
+ticker = getTicker()
+data = requestData(ticker)
 plotChart(data)
