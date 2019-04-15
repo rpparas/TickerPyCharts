4430e9be25b5f375581ed1a121c5e9196b082cb5
Rommel Paras <rpparas@gmail.com>
2019-04-14 19:39:34 -0700

    Added delays in between some test cases
    Attempting to test and get around rate limits

--- TickerChartUnitTest.py
+++ TickerChartUnitTest.py
@@ -1,30 +1,39 @@
 from TickerChart import TickerChart
+import time
 
 tc = TickerChart()
 tc.setDebugMode(True)
-
+delay = 10
 # Unit tests for stocks
 tc.setSeriesType('S')
-assert tc.validateTicker('AAPL') == 1
-assert tc.validateTicker('GOOOOGLE') == 0
-assert tc.validateTicker('GOOGLE') == -1
-assert tc.validateTicker(None) == 0
-assert tc.validateTicker('')  == 0
-assert tc.validateTicker('AF.PAR') == 1
+tests = {
+    'AAPL': 1,
+    'GOOOOGLE': 0,
+    'GOOGLE': -1,
+    None: 0,
+    '': 0,
+    'AF.PAR': 1,
+}
+
+for inp, out in tests.items():
+    assert tc.validateTicker(inp) == out
+    time.sleep(delay) # Add delay due to rate limit imposed by API
 
 tc.validateTicker('Air France')
 assert len(tc.getMatchingTickers()) > 0
+time.sleep(delay)
 
 tc.validateTicker('     ABCDEFG     ')
 assert len(tc.getMatchingTickers()) == 0
+time.sleep(delay)
 
-
-# Unit tests for cryptocurrencies
+# # Unit tests for cryptocurrencies
 tc.setSeriesType('C')
+
 assert tc.validateTicker('ETH') == 1
 assert tc.validateTicker(' xrp  ') == 1
 assert tc.validateTicker('btc') == 1
-assert tc.validateTicker('808       ') == 1
+assert tc.validateTicker('808') == 1
 assert tc.validateTicker(' dunno ') == -3
 
 
@@ -33,5 +42,4 @@ tc.setSeriesType('F')
 assert tc.validateTicker('USD') == 1
 assert tc.validateTicker('Eur') == 1
 assert tc.validateTicker('LOL') == -3
-
 
