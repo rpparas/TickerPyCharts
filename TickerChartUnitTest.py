7ba68c1f3570d774c19ceb70b7eb36bc2bdc67ff
Rommel Paras <rpparas@gmail.com>
2019-04-14 19:06:25 -0700

    Added test cases to validate currencies

--- TickerChartUnitTest.py
+++ TickerChartUnitTest.py
@@ -2,11 +2,24 @@ from TickerChart import TickerChart
 
 tc = TickerChart()
 tc.setDebugMode(True)
-tc.setSeriesType('S')
 
+tc.setSeriesType('S')
 assert tc.validateTicker('AAPL') == 1
 assert tc.validateTicker('GOOOOGLE') == 0
 assert tc.validateTicker('GOOGLE') == -1
 assert tc.validateTicker(None) == 0
 assert tc.validateTicker('')  == 0
 assert tc.validateTicker('AF.PAR') == 1
+
+
+tc.setSeriesType('C')
+assert tc.validateTicker('ETH') == 1
+assert tc.validateTicker(' xrp  ') == 1
+assert tc.validateTicker('btc') == 1
+assert tc.validateTicker(' dunno ') == -3
+
+
+tc.setSeriesType('F')
+assert tc.validateTicker('USD') == 1
+assert tc.validateTicker('Eur') == 1
+assert tc.validateTicker('LOL') == -3
